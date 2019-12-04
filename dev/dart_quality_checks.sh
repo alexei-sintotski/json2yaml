ff616201c7b08d8e31ddadafcbe376bde1552dfa
Alexei Sintotski <alexei.sintotski@gmail.com>
2019-12-04 23:13:12 +0100

    Initial content

---
+++ dev/dart_quality_checks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+dartanalyzer --fatal-hints .
+pub run test
+pub run dependency_validator --ignore=functional_data_generator,sum_types_generator,test_coverage
+dev/format_dart_code.sh --set-exit-if-changed
+dev/generate_code.sh
+pub publish --dry-run
