c73c848db017d034c2faeca83c654b8a422a00bd
Alexei Sintotski <alexei.sintotski@gmail.com>
2020-12-12 16:08:47 +0100

    Dart 2.10 support + upgrade of dependencies

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -3,6 +3,6 @@
 dart example/main.dart
 dartanalyzer --fatal-hints .
 pub run test
-pub run dependency_validator --ignore=functional_data_generator,sum_types_generator,test_coverage
+pub run dependency_validator
 dev/format_dart_code.sh --set-exit-if-changed
 pub publish --dry-run
