d1a60d3f280ff4f867022a758de018110671c853
Alexei Sintotski <alexei.sintotski@gmail.com>
2020-12-13 10:23:10 +0100

    Fix code quality against recently introduced Dart analyzer options

--- lib/src/json2yaml.dart
+++ lib/src/json2yaml.dart
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,12 +22,15 @@
  * SOFTWARE.
  */
 
-import 'package:json2yaml/src/internal/render_to_yaml.dart';
-
 /// Yaml formatting control options
 enum YamlStyle {
+  /// Default formatting style applicable to most of situations
   generic,
+
+  /// YAML formatting style following pubspec.yaml formatting conventions
   pubspecYaml,
+
+  /// YAML formatting style following pubspec.lock formatting conventions
   pubspecLock,
 }
 
@@ -36,4 +39,67 @@ String json2yaml(
   Map<String, dynamic> json, {
   YamlStyle yamlStyle = YamlStyle.generic,
 }) =>
-    renderToYaml(json, 0, yamlStyle);
+    _renderToYaml(json, 0, yamlStyle);
+
+String _renderToYaml(
+  Map<String, dynamic> json,
+  int nestingLevel,
+  YamlStyle style,
+) =>
+    json.entries
+        .map((entry) => _formatEntry(
+              entry,
+              nestingLevel,
+              style,
+            ))
+        .join('\n');
+
+String _formatEntry(
+        MapEntry<String, dynamic> entry, int nesting, YamlStyle style) =>
+    '${_indentation(nesting)}${entry.key}:${_formatValue(
+      entry.value,
+      nesting,
+      style,
+    )}';
+
+String _formatValue(dynamic value, int nesting, YamlStyle style) {
+  if (value is Map<String, dynamic>) {
+    return '\n${_renderToYaml(value, nesting + 1, style)}';
+  }
+  if (value is List<dynamic>) {
+    return '\n${_formatList(value, nesting + 1, style)}';
+  }
+  if (value is String) {
+    if (_isMultilineString(value)) {
+      return ' |\n${value.split('\n').map(
+            (s) => '${_indentation(nesting + 1)}$s',
+          ).join('\n')}';
+    }
+    if (_containsSpecialCharacters(value) ||
+        (_containsFloatingPointPattern(value) &&
+            style != YamlStyle.pubspecYaml)) {
+      return ' "$value"';
+    }
+  }
+  if (value == null) {
+    return '';
+  }
+  return ' $value';
+}
+
+String _formatList(List<dynamic> list, int nesting, YamlStyle style) => list
+    .map((dynamic value) =>
+        '${_indentation(nesting)}-${_formatValue(value, nesting + 2, style)}')
+    .join('\n');
+
+String _indentation(int nesting) => _spaces(nesting * 2);
+String _spaces(int n) => ''.padRight(n, ' ');
+
+bool _isMultilineString(String s) => s.contains('\n');
+
+bool _containsFloatingPointPattern(String s) =>
+    s.contains(RegExp(r'[0-9]\.[0-9]'));
+
+bool _containsSpecialCharacters(String s) =>
+    _specialCharacters.any((c) => s.contains(c));
+final _specialCharacters = r':{}[],&*#?|-<>=!%@\'.split('');
